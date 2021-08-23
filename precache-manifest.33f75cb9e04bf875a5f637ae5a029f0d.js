6cdff3af7952968dd05da4319b71ae6978333c46
hassancode1 <hassanjaeh@gmail.com>
2021-08-23 03:38:08 -0700

    Updates

---
+++ precache-manifest.33f75cb9e04bf875a5f637ae5a029f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3ba4ad51000710355dde4b25a486e2",
+    "url": "/htttp//hassancode1.github.io/youtube-clone/index.html"
+  },
+  {
+    "revision": "b7efbf2fa90084242602",
+    "url": "/htttp//hassancode1.github.io/youtube-clone/static/css/main.c35e3c04.chunk.css"
+  },
+  {
+    "revision": "6e693023353a633d2095",
+    "url": "/htttp//hassancode1.github.io/youtube-clone/static/js/2.df578bf8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/htttp//hassancode1.github.io/youtube-clone/static/js/2.df578bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7efbf2fa90084242602",
+    "url": "/htttp//hassancode1.github.io/youtube-clone/static/js/main.51250903.chunk.js"
+  },
+  {
+    "revision": "28ca64084ddb42ceaacf",
+    "url": "/htttp//hassancode1.github.io/youtube-clone/static/js/runtime-main.c3e99176.js"
+  }
+]);
